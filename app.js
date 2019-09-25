d596beeaadc50c9d2caf999a46a6e4e637fb8814
Annemarie Hin <ame5033@gmail.com>
2019-09-25 11:52:53 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBjRyHOGQ2OkrENe0oG6i41hiXD7NzPEtk",
+    authDomain: "train-schedule-app-48a5e.firebaseapp.com",
+    databaseURL: "https://train-schedule-app-48a5e.firebaseio.com",
+    projectId: "train-schedule-app-48a5e",
+    storageBucket: "",
+    messagingSenderId: "13718150236",
+    appId: "1:13718150236:web:d925990c4c89df286b1a99"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
